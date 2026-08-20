e2d04fd6b869110957ae02f2034106b495d3130e
mczoomzo-design <mczoomzo@gmail.com>
2026-08-20 16:51:23 +0700

    Update API_URL to the deployed Google Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // วาง Web App URL ที่ได้จากขั้นตอน Deploy ของ Google Apps Script ตรงนี้
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycb..../exec
-window.API_URL = 'PASTE_YOUR_GAS_WEB_APP_URL_HERE';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbwny9HTRvLXjnp95h4e7Ye-Qvi3Fvxl0zpRHYwnFF6IayNdfelsa4pJbFHMu4CbuHOB/exec';
